6fbdff439fa5ee4a29845cd6ee944efbdb2cb8db
BJ Tan <88465305+BJTangerine@users.noreply.github.com>
2022-02-15 14:05:18 -0700

    Add files via upload

---
+++ Multiple Subqueries on Soccer Data.sql
@@ -0,0 +1,16 @@
+SELECT 
+	stage,
+    ROUND(s.avg_goals,2) AS avg_goal,
+    (SELECT AVG(home_goal + away_goal) FROM match WHERE season = '2012/2013') AS overall_avg -- yields the average goals scored in the 2012/2013 season
+
+FROM 
+	(SELECT
+		 stage,
+         AVG(home_goal + away_goal) AS avg_goals
+	 FROM match
+	 WHERE season = '2012/2013'
+	 GROUP BY stage) AS s -- calculates the average goals scored in each stage during the 2012/2013 season
+
+WHERE 
+	s.avg_goals > (SELECT AVG(home_goal + away_goal) 
+                    FROM match WHERE season = '2012/2013'); --Filters for stages where the average goals exceeds the overall average in 2012/2013
